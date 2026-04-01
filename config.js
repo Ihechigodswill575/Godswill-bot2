3d1f9c5f17049e8b194ec12b75319ff6c8be4df3
Ihechigodswill575 <Ihechigodswill575@gmail.com>
2026-04-01 11:06:21 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
+
 'use strict'
 
 const rawOwners = process.env.OWNER_NUMBERS || process.env.OWNER_NUMBER || '2348145688688'
@@ -5,7 +6,7 @@ const OWNER_NUMBERS = rawOwners.split(',').map(n => n.replace(/[^0-9]/g, '').rep
 
 module.exports = {
     BOT_NAME      : process.env.BOT_NAME   || 'TAVIK BOT',
-    BOT_VERSION   : 'V2.0',
+    BOT_VERSION   : 'V3.0',
     OWNER_NAME    : process.env.OWNER_NAME || 'TAVIK(GODSWILL)',
     OWNER_NUMBER  : OWNER_NUMBERS[0],
     OWNER_NUMBERS,
@@ -14,7 +15,10 @@ module.exports = {
     EVO_KEY      : process.env.EVO_KEY      || '57695a473ad9b3f8c0f7ffee6f15dee960f35f5fc18dd903d36276150a3c17b4',
     EVO_INSTANCE : process.env.EVO_INSTANCE || 'tavik-bot',
 
-    UNSPLASH_KEY : process.env.UNSPLASH_KEY || '',
-    PREFIX       : '.',
-    BAD_WORDS    : ['fuck', 'shit', 'bitch', 'bastard', 'idiot', 'stupid'],
+    PREFIX    : '.',
+    BAD_WORDS : ['fuck', 'shit', 'bitch', 'bastard', 'idiot', 'stupid'],
+
+    // Rate limit: max messages per window
+    CHATBOT_RATE_LIMIT     : 20,      // max messages
+    CHATBOT_RATE_WINDOW_MS : 30_000,  // per 30 seconds
 }
